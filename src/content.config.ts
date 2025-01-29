c92336b8ab861b25d9a141a9e0b972eb962db997
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2025-01-29 14:10:59 +0000

    Update schema for calendar events

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,43 @@
 import { file } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
+const eventTypes = [
+    "festival",
+    "party",
+    "regular_course",
+    "workshop",
+] as const;
+
+const eventTypeInfoSchema = z.union([
+    z.boolean(),
+    z.object({ requires_registration: z.boolean() })
+]).optional();
+
+type EventTypes = {
+    [EventType in typeof eventTypes[number]]: typeof eventTypeInfoSchema
+};
+
+let eventTypeSchemaStructure: EventTypes = {} as EventTypes
+
+eventTypes.forEach((eventType) => {
+    eventTypeSchemaStructure[eventType] = eventTypeInfoSchema;
+})
+
 const eventTypeSchema = z.object({
-    festival: z.boolean().optional(),
-    party: z.boolean().optional(),
-    regular_course: z.boolean().optional(),
-    workshop: z.boolean().optional(),
+    ...eventTypeSchemaStructure,
     requires_registration: z.boolean().optional(),
+}).transform((arg) => {
+    let result: { [EventType in typeof eventTypes[number]]?: { requires_registration: boolean } } = {};
+    eventTypes.forEach((eventType) => {
+        if (arg[eventType]) {
+            result[eventType] = {
+                requires_registration: (typeof arg[eventType] === 'object'
+                    ? arg[eventType].requires_registration
+                    : arg.requires_registration) ?? false
+            };
+        }
+    });
+    return result;
 });
 
 const eventBaseSchema = z.object({
@@ -16,19 +47,27 @@ const eventBaseSchema = z.object({
     time: z.string().time().optional(),
     duration: z.string().time(),
     location: z.string(),
+    links: z.object({
+        homepage: z.string().optional(),
+        facebook: z.string().optional(),
+        instagram: z.string().optional(),
+        community: z.string().optional(),
+    }).optional(),
     url: z.string().optional(),
     type: eventTypeSchema,
 });
 
+const eventSchema = eventBaseSchema.extend({
+    groups: z.array(z.object({
+        title: z.string(),
+        children: z.array(eventBaseSchema).optional()
+    })).optional(),
+    children: z.array(eventBaseSchema).optional()
+});
+
 const calendar = defineCollection({
     loader: file("src/data/calendar.yaml"),
-    schema: eventBaseSchema.extend({
-        groups: z.array(z.object({
-            title: z.string(),
-            children: z.array(eventBaseSchema).optional()
-        })).optional(),
-        children: z.array(eventBaseSchema).optional()
-    })
+    schema: eventSchema
 });
 
 export const collections = { calendar }
